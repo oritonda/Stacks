4fdc1763f17ee5fe76b1fa9c6ff842614efca4fb
oritonda ramugondo <oritonda@thedigitalacademy.co.za>
2021-05-25 10:23:03 +0200

    admin starting point

--- client/Admin/src/app/app-routing.module.ts
+++ client/Admin/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
+import { SignupComponent } from './components/signup/signup.component';
+import { SigninComponent } from './components/signin/signin.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { LandingComponent } from './components/landing/landing.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'landing',component:LandingComponent},
+  {path:'dashboard',component:DashboardComponent},
+  {path:'signin',component:SigninComponent},
+  {path:'signup',component:SignupComponent},
+  {path:'**',redirectTo:'landing',pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/Admin/src/app/app.module.ts
+++ client/Admin/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { SigninComponent } from './components/signin/signin.component';
+import { SignupComponent } from './components/signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    DashboardComponent,
+    SigninComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
